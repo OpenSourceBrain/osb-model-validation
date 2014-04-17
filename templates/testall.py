2d9826c44511e830e99e5c18d343846e3bdad2c0
Boris Marin <borismarin@gmail.com>
2014-04-17 14:08:24 +0100

    finding all omt files in project, recursively

--- templates/testall.py
+++ templates/testall.py
@@ -1,16 +1,18 @@
 from sys import argv
-from os import environ
-from glob import glob
+from os import environ, getcwd
+from pathlib import Path 
 from osb_test.parse_ost import parse_ost, load_yaml
 
 def test_all():
+    cwd = Path(getcwd())
+    all_omts = [p.as_posix() for p in cwd.glob('**/*.omt')]
     if environ.get('TRAVIS'):
         engine = environ.get('OST_ENGINE').lower()
-        tsts = [all(parse_ost(f))
-                for f in glob('*.ost') 
-                if load_yaml(f)['engine'].lower() == engine]
+        tsts = [all(parse_ost(t))
+                for t in all_omts 
+                if load_yaml(t)['engine'].lower() == engine]
     else:
-        tsts= [parse_ost(f) for f in glob('*.ost')]
+        tsts= [all(parse_ost(t)) for t in all_omts]
     assert all(tsts)
 
 
