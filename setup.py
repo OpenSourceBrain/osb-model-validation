c2f33d7df782f3fe9c99054b849251bd0784ae5d
Boris Marin <borismarin@gmail.com>
2014-04-22 18:46:22 +0100

    installing shell scripts to run standalone tests

--- setup.py
+++ setup.py
@@ -7,24 +7,23 @@ setup(
     author='Boris Marin',
     author_email='borismarin@gmail.com',
     packages=find_packages(),
-    entry_points = {'console_scripts':
-                    ['validate_mep = omv.validation.validate_mep:main']},
-    package_data = {'omv':['schemata/mep.yaml',
-                           'schemata/types/*.yaml',
-                           'schemata/types/base/*.yaml']},
+    entry_points={
+        'console_scripts': [
+            'omv_validate_mep = omv.validation.validate_mep:main',
+            'omv_alltests = omv.test_finder:test_all',
+            'omv_test = omv.test_finder:test_one']},
+    package_data={
+        'omv': [
+            'schemata/mep.yaml',
+            'schemata/types/*.yaml',
+            'schemata/types/base/*.yaml']},
     url='http://opensourcebrain.org/OSBModelValidation',
     license='LICENSE.txt',
     description='OpenSourceBrain Model Validation',
     long_description=open('README.md').read(),
-    install_requires=['PyYAML', 'numpy', 'pyrx', 'pathlib'],
+    install_requires=[
+        'PyYAML',
+        'numpy',
+        'pyrx',
+        'pathlib'],
 )
-
-
-
-
-
-
-
-
-
-
