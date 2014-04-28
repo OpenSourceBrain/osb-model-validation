3e517b3796a54e71708eb7b2d451f181a38b43ff
Boris Marin <borismarin@gmail.com>
2014-04-28 15:58:55 +0100

    mininal consistency for log msgs

--- omv/analyzers/resting.py
+++ omv/analyzers/resting.py
@@ -8,10 +8,10 @@ class RestingAnalyzer(OMVAnalyzer):
     def parse_resting(self, to_parse):
         if isinstance(to_parse, (int, float)):
             resting = to_parse
-            inform('Explicit resting potential specified', to_parse, indent=1)
+            inform('Explicit resting potential specified:', to_parse, indent=1)
         elif 'file' in to_parse:
             f = fn.FileNodeHelper(to_parse['file'])
-            inform('Calculating resting potential from file', f.filename, indent = 1)
+            inform('Calculating resting potential from file:', f.filename, indent = 1)
             window = to_parse.get('average last', 1)
             if window > 1:
                 inform('Number of final points taken for averaging:', window, indent = 2)
--- omv/analyzers/spikes.py
+++ omv/analyzers/spikes.py
@@ -11,12 +11,12 @@ class SpikeAnalyzer(OMVAnalyzer):
         elif 'file' in to_parse:
             f = fn.FileNodeHelper(to_parse['file'])
             tv = f.get_timeseries()
-            inform('Reading timeseries from', f, indent=1)
+            inform('Reading timeseries from:', f, indent=1)
             if 'spike detection' in to_parse:
                 sd = to_parse['spike detection']
                 method = sd.get('method', 'threshold')
                 threshold = float(sd.get('threshold', 0))
-                inform('Detecting spikes with method', method, indent=2)
+                inform('Detecting spikes with method:', method, indent=2)
                 spikes = ts.spikes_from_timeseries(tv, method=method, threshold=threshold)
         return spikes
 
