6a943168889627e0ac0618bd2080abdf0aaf7c28
Padraig Gleeson <p.gleeson@gmail.com>
2014-04-29 16:05:04 +0100

    Update getjnml.py

--- omv/backends/getjnml.py
+++ omv/backends/getjnml.py
@@ -7,4 +7,4 @@ def install_jnml():
     jnmlpath = os.path.join(os.environ['HOME'],'jnml')
     os.mkdir(jnmlpath)
     with working_dir(jnmlpath):
-        print co(['svn', 'checkout', 'svn://svn.code.sf.net/p/neuroml/code/jNeuroMLJar'])
+        print co(['svn', 'checkout', 'https://svn.code.sf.net/p/neuroml/code/jNeuroMLJar'])
