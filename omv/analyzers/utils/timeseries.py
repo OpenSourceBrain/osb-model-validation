f59e061d4eebc5088bb9f25df120d1bbccde90d2
Boris Marin <borismarin@gmail.com>
2014-04-30 18:29:07 +0100

    added bound checker

--- omv/analyzers/utils/timeseries.py
+++ omv/analyzers/utils/timeseries.py
@@ -15,10 +15,10 @@ def detect_spikes(v, method='threshold', threshold=0.):
         #see for example scipy.signal.find_peaks_cwt 
     return extrema
 
-def load_data_file(fname, columns=(0,1), header_lines=0, scaling=(1,1)):
+def load_data_file(fname, columns=(0,1), header_lines=0, scaling=1):
     from numpy import loadtxt
     ts = loadtxt(fname, usecols=columns, skiprows=header_lines)
-    return (ts * scaling).T #scratch your heads, matlab users!
+    return ts * scaling
 
 def compare_arrays(arrays, tolerance):
     from numpy import allclose, array
@@ -32,7 +32,6 @@ def compare_dictionaries(d1, d2, tolerance=0.1):
     a2 = array([d2[k] for k in ks if k in d2])
     return allclose(a1, a2, tolerance)
 
-
 def load_spike_times(spiketime):
     if isinstance(spiketime, list):
         return spiketime
@@ -42,7 +41,7 @@ def load_spike_times(spiketime):
 def spikes_from_timeseries(ts, **kwargs):
     method = kwargs.get('method', 'threshold')
     threshold = kwargs.get('threshold', 0)
-    t, v = ts
+    t, v = ts.T
     spk_idx = detect_spikes(v, method, threshold)
     return t[spk_idx]
 
@@ -51,9 +50,17 @@ def spikes_from_datafile(path, columns=(0,1), header=0, method='threshold', thre
     spk_idx = detect_spikes(v, method, threshold)
     return t[spk_idx]
 
-def average_resting(tv, window):
+def average_resting(tv, window, col=1):
     from numpy import mean
-    return mean(tv[1, -window:])
+    return mean(tv[-window:,col])
+    
+def all_within_bounds(ts, bounds=(0,1)):
+    from numpy import all
+    return all((ts[:,1:]>=bounds[0]) & (ts[:,1:]<=bounds[1]))
+    
+def all_nonzero(ts):
+    from numpy import all
+    return all(ts[:,1:])
 
 def test_detect_spikes():
     from numpy import array, all, arange
@@ -64,6 +71,9 @@ def test_detect_spikes():
 
     spk_idx = detect_spikes(x, method='threshold', threshold=0.1)
     assert all(spk_idx == arange(1,len(x),4))
+
+
+
 
 
 
