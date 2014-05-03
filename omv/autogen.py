931fca22b8084e461c06f7b3afbdc35000c44ff5
Boris Marin <borismarin@gmail.com>
2014-05-03 11:45:32 +0100

    more logic for autogen

--- omv/autogen.py
+++ omv/autogen.py
@@ -1,4 +1,18 @@
-from os.path import isdir
+from os.path import isdir, join
+from glob import glob
+
+
+def read_option(options, default=0):
+    print [(i, opt) for i, opt in enumerate(options)]
+    sel = None
+    while not sel:
+        try:
+            sel = int(raw_input('option [%s]' % default))
+            if sel is '':
+                sel = 0
+        except IndexError:
+            print 'invalid index!'
+    return options[sel]
 
 def autogen():
     dirs_to_engines_exts = {'NEURON': {'engine':'NEURON', 'extension':'.hoc'},
@@ -6,8 +20,14 @@ def autogen():
 
     for d, eng_ext in dirs_to_engines_exts.iteritems():
         if isdir(d):
-            print d, 'found'
-                            
+            engine = dirs_to_engines_exts[d]['engine']
+            ext = dirs_to_engines_exts[d]['extension']
+            print 'Default directory for {0} engine found.'.format(engine)
+            print '  Will look for scripts with {0} extension'.format(ext)
+            scripts = glob(join(d,'*'+ext))
+            print '    found', scripts
+            script = read_option(scripts)
+            print 'selected', script 
 
 
 if __name__ == '__main__':
