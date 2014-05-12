ca8f29974c40c990ffe0c838ff479fe4429a4a30
Boris Marin <borismarin@gmail.com>
2014-05-12 12:01:20 +0100

    better logging for getnrn script

--- omv/backends/getnrn.py
+++ omv/backends/getnrn.py
@@ -1,33 +1,56 @@
 import os
 import sys
-from subprocess import check_output as co
+from subprocess import CalledProcessError, STDOUT, check_output as co
 from distutils.core import run_setup
 
 from utils.wdir import working_dir
 
+def run_n_log(cmd):
+    try:
+        co(cmd, stderr=STDOUT)
+    except CalledProcessError as cperr:
+        print '#ERROR: command [', ' '.join(cperr.cmd), '] failed:'
+        print cperr.output
+        sys.exit(1)
 
-def install_neuron(get_latest=False):
+
+
+def install_neuron(get_latest=True):
     nrnpath = os.path.join(os.environ['HOME'],'neuron')
     os.mkdir(nrnpath)
     with working_dir(nrnpath):
         if get_latest:
-            print co(['hg', 'clone', 'http://www.neuron.yale.edu/hg/neuron/nrn'])
+            print 'Getting latest NEURON source...',
+            run_n_log(['hg', 'clone', 'http://www.neuron.yale.edu/hg/neuron/nrn'])
             os.chdir('nrn')
-            print co(['./build.sh'])
+            run_n_log(['./build.sh'])
+            print 'done!'
         else:
-            print co(['wget', 'http://www.neuron.yale.edu/ftp/neuron/versions/v7.3/nrn-7.3.tar.gz'])
-            print co(['tar', 'xzvf', 'nrn-7.3.tar.gz'])
-            print co(['mv', 'nrn-7.3', 'nrn'])
+            print 'Getting NEURON 7.3...', 
+            run_n_log(['wget', 'http://www.neuron.yale.edu/ftp/neuron/versions/v7.3/nrn-7.3.tar.gz'])
+            run_n_log(['tar', 'xzvf', 'nrn-7.3.tar.gz'])
+            run_n_log(['mv', 'nrn-7.3', 'nrn'])
             os.chdir('nrn')
+            print 'done!'
             
+        print 'CWD:', os.getcwd()
         path = os.getcwd()
         pyexec = sys.executable
-        co(["./configure --prefix=%s --without-iv --with-nrnpython=%s"%(path,pyexec)], shell=True)
-        print co(['make'])
-        print co(['make', 'install'])
+        print 'Building NEURON...',
+        run_n_log(['./configure', '--prefix=%s'%path, '--without-iv', '--with-nrnpython=%s'%pyexec, '--enable-pysetup'])
+        run_n_log(['make'])
+        run_n_log(['make', 'install'])
+        print 'done!'
 
-        os.chdir('src/nrnpython')
-        run_setup('./setup.py', ['install'])
+        #print 'Installing pyNEURON...', 
+        #os.chdir('src/nrnpython')
+        #run_setup('./setup.py', ['install'])
+        #print 'done!'
+
+
+
+
+
 
 
 
