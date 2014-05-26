8d71628b8b66faa6ac25a8b0d1bee690751d2a54
Boris Marin <borismarin@gmail.com>
2014-05-26 17:41:43 +0100

    pep8ing omv_util

--- omv/omv_util.py
+++ omv/omv_util.py
@@ -27,7 +27,7 @@ from backends import OMVBackends
 
 def main():
     arguments = docopt(__doc__, version='OpenSourceBrain Model Validation 0.0')
-    
+
     if arguments['test']:
         test_one(arguments['<testMe.omt>'])
 
@@ -47,7 +47,7 @@ def main():
         else:
             print 'Will install', arguments['<backend>']
             print 'Backend installation not implemented yet!'
-        
+
     elif arguments['list-backends']:
         for be in OMVBackends.keys():
             print be
@@ -57,28 +57,7 @@ def main():
         dry = arguments['--dryrun']
         auto = arguments['-y']
         autogen(auto, dry)
-    
-    
+
+
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
