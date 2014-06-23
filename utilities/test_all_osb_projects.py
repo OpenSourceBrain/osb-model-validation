a7474f848b3692aa0e0c0374a2f9220577c2aba8
Padraig Gleeson <p.gleeson@gmail.com>
2014-06-23 14:22:40 +0100

    Some more updates on test all script

--- utilities/test_all_osb_projects.py
+++ utilities/test_all_osb_projects.py
@@ -5,7 +5,12 @@ Clone all OSB repos using OMV & test locally
 
         
 import sys
+import os
 import osb
+import shutil
+import datetime
+from subprocess import check_output as co
+
 
 testable_projects = 0
 passing_projects = 0
@@ -13,10 +18,18 @@ projects = 0
 
 test_dir = "local_test"
 
+fresh_clones = True
 
+
+if fresh_clones: 
+    if os.path.exists(test_dir):
+        shutil.rmtree(test_dir)
+    
+os.makedirs(test_dir)
 
 if __name__ == "__main__":
-    
+    start = datetime.datetime.now()
+
     project_num = 1000
     if len(sys.argv) == 2:
         project_num = int(sys.argv[1])
@@ -27,25 +40,35 @@ if __name__ == "__main__":
 
         github_repo = project.github_repo
 
-	projects +=1
-	passed = 1
+        projects +=1
 
 
-	if github_repo is not None:
+        if github_repo is not None:
             identifier = project.identifier
 
-	    if github_repo.check_file_in_repository(".travis.yml"):
-                print("  .travis.yml found!")
-                testable_projects +=1
+            if github_repo.check_file_in_repository(".travis.yml"):
+
+                raw_url = github_repo.link_to_raw_file_in_repo(".travis.yml")
+                print("  .travis.yml found at %s"%raw_url)
+                contents = osb.get_page(raw_url)
+                if not 'omv' in contents:
+                    print("That .travis.yml does not look like is uses OMV...")
+                else:
+                    testable_projects +=1
+                    target_dir = '%s/%s'%(test_dir, project.identifier)
+                    print co(['git', 'clone', project.github_repo_str, target_dir])
+                    print co(['omv', 'all'], cwd=target_dir)
+                    passing_projects +=1
 
             else:
-		    print("  (No .travis.yml)")
+                print("  (No .travis.yml)")
 
-	else:
-	    print("  (No GitHub repository)")
-
-
+        else:
+            print("  (No GitHub repository)")
 
 
-    print("\nNumber of projects: %i, of which %i have tests and %i passed\n"%(projects, testable_projects, passing_projects))
+
+    end = datetime.datetime.now()
+
+    print("\n%i projects checked, of which %i have OMV test(s) and %i passed in %s seconds\n"%(projects, testable_projects, passing_projects, (end-start).seconds))
 
