953b151fa085ec4071bec9c9ee3ddd8330994058
Boris Marin <borismarin@gmail.com>
2014-06-24 12:16:31 +0100

    replacing OMT with OMV

--- omv/find_tests.py
+++ omv/find_tests.py
@@ -1,4 +1,3 @@
-from sys import argv, exit
 from os import environ, getcwd
 from pathlib import Path 
 from omv.parse_omt import parse_omt, load_yaml
@@ -7,7 +6,7 @@ def test_all():
     cwd = Path(getcwd())
     all_omts = [p.as_posix() for p in cwd.glob('**/*.omt')]
     if environ.get('TRAVIS'):
-        engine = environ.get('OST_ENGINE').lower()
+        engine = environ.get('OMV_ENGINE').lower()
         tsts = [all(parse_omt(t))
                 for t in all_omts 
                 if load_yaml(t)['engine'].lower() == engine]
