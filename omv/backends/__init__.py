3363d58f50c81047e6f6eff5d7e61445d186d838
Padraig Gleeson <p.gleeson@gmail.com>
2014-06-25 14:35:12 +0100

    Removing be_paths/be_env_vars from __init__.py

--- omv/backends/__init__.py
+++ omv/backends/__init__.py
@@ -9,12 +9,3 @@ OMVBackends = {
     JNeuroMLNRNBackend.name : JNeuroMLNRNBackend,
     JLemsBackend.name : JLemsBackend
 }
-
-be_paths = {NeuronBackend.name: "$HOME/neuron/nrn/`arch`/bin",
-            JNeuroMLBackend.name: "$HOME/jnml/jNeuroMLJar",
-            JNeuroMLNRNBackend.name: "$HOME/jnml/jNeuroMLJar:$HOME/neuron/nrn/`arch`/bin",
-            JLemsBackend.name: "$HOME/jLEMS"}
-            
-be_env_vars = {JNeuroMLBackend.name:"JNML_HOME=$HOME/jnml/jNeuroMLJar",
-               'jNeuroML_NEURON':"JNML_HOME=$HOME/jnml/jNeuroMLJar",
-               JLemsBackend.name:"LEMS_HOME=$HOME/jLEMS"}
