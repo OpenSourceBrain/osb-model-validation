32dec46327ece4831140f93dd0e12d0b2402d405
Boris Marin <borismarin@gmail.com>
2014-07-04 20:16:30 +0100

    added getgenesis

---
+++ omv/backends/getgenesis.py
@@ -0,0 +1,24 @@
+import os
+from subprocess import check_output as co
+
+from utils.wdir import working_dir
+
+def install_genesis(get_latest=False):
+    genpath = os.path.join(os.environ['HOME'],'genesis')
+    os.mkdir(genpath)
+    with working_dir(genpath):
+        print co(['wget', 'https://github.com/borismarin/omv-testbed/blob/master/genesis23omv.tgz?raw=true'])
+        print co(['tar', 'xzvf', 'genesis23omv.tgz'])
+        os.chdir('genesis/src')
+        print co(['make'])
+        print co(['cp', 'startup/travis_simrc', os.path.join(os.environ['HOME'], '.simrc')])
+
+
+
+
+
+
+
+
+
+
