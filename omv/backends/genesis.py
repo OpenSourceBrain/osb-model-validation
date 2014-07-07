9e44b753786c7a273be5681fea4ab76eee7a1ffd
Boris Marin <borismarin@gmail.com>
2014-07-07 20:15:14 +0100

    more functionality for the genesis b-e

--- omv/backends/genesis.py
+++ omv/backends/genesis.py
@@ -1,16 +1,19 @@
 import re
+import os
 import subprocess as sp
+from tempfile import NamedTemporaryFile
 
 from backend import OMVBackend
+from utils.wdir import working_dir
 
 class GenesisBackend(OMVBackend):
     
     name = "genesis"
 
-    @staticmethod
-    def is_installed(version):
+    @classmethod
+    def is_installed(cls, version):
         from tempfile import NamedTemporaryFile
-        print("Checking whether %s is installed..." % GenesisBackend.name)
+        print("Checking whether %s is installed..." % cls.name)
 
         ret = True
         try:
@@ -21,13 +24,42 @@ class GenesisBackend(OMVBackend):
             m = re.search('version:'+'\s*([0-9]*\.?[0-9]+)\s*', out)
             if m:
                 ret = m.groups()[0]
-
         except OSError:
-            ret =  False
+            ret = False
         finally:
             temp.close()
+
         return ret
 
+    @classmethod
+    def install(cls, backend_version):
+        import getgenesis
+        home = os.environ['HOME']
+        cls.path = os.path.join(home, 'genesis', 'src')
+        print 'Will fetch and install genesis-2.3'
+        getgenesis.install_genesis()
 
+    def run(self):
+        with working_dir(os.path.dirname(self.modelpath)):
+            try:
+                temp = NamedTemporaryFile(suffix='.g')
+                temp.write('include %s\n' % self.modelpath)
+                temp.write('\n'.join(self.extra_pars))
+                temp.seek(0)
+
+                with open('/tmp/omv.gen.err', 'w+') as se:
+                    self.stdout = sp.check_output(['genesis', '-nox',
+                                                   '-batch', '-notty', temp.name],
+                                                  stderr=se)
+                    se.seek(0)
+                    self.stderr = se.readlines()
+                self.returncode = 0
+            except sp.CalledProcessError as e:
+                self.returncode = e.returncode
+            finally:
+                temp.close()
+            
+    def build_query_string(self, name, cmd):
+        return '{%s; echo %s: {%s}' % (cmd, name, name)
 
 
