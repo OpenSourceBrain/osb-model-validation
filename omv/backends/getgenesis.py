73df25daac2618b0497e3c53ccfb3aa410f68e71
Boris Marin <borismarin@gmail.com>
2014-07-07 20:29:49 +0100

    getting raw tgz

--- omv/backends/getgenesis.py
+++ omv/backends/getgenesis.py
@@ -7,10 +7,8 @@ def install_genesis(get_latest=False):
     genpath = os.path.join(os.environ['HOME'],'genesis')
     os.mkdir(genpath)
     with working_dir(genpath):
-        print co(['wget', 'https://github.com/borismarin/omv-testbed/blob/master/genesis23omv.tgz'])
+        print co(['wget', 'https://github.com/borismarin/omv-testbed/raw/master/genesis23omv.tgz'])
         print co(['pwd'])
-        print co(['ls', '-la'])
-        print co(['ls', '-la', genpath])
         print co(['tar', 'xzvf', 'genesis23omv.tgz'])
         os.chdir('genesis/src')
         print co(['make'])
