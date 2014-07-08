de6eb0c4bff92b384a5e5e0abd4f7b51162bb930
Boris Marin <borismarin@gmail.com>
2014-07-08 18:10:56 +0100

    added area and temp handling to genesis b-e

--- omv/backends/genesis.py
+++ omv/backends/genesis.py
@@ -6,8 +6,9 @@ from tempfile import NamedTemporaryFile
 from backend import OMVBackend
 from utils.wdir import working_dir
 
+
 class GenesisBackend(OMVBackend):
-    
+
     name = "genesis"
 
     @classmethod
@@ -20,8 +21,9 @@ class GenesisBackend(OMVBackend):
             temp = NamedTemporaryFile(suffix='.g')
             temp.writelines(['echo "version: "{version} \n', 'quit \n'])
             temp.seek(0)
-            out = sp.check_output(['nxgenesis', '-nox', '-batch', '-notty', temp.name])
-            m = re.search('version:'+'\s*([0-9]*\.?[0-9]+)\s*', out)
+            out = sp.check_output(
+                ['nxgenesis', '-nox', '-batch', '-notty', temp.name])
+            m = re.search('version:' + '\s*([0-9]*\.?[0-9]+)\s*', out)
             if m:
                 ret = m.groups()[0]
         except OSError:
@@ -40,11 +42,14 @@ class GenesisBackend(OMVBackend):
         getgenesis.install_genesis()
 
     def run(self):
+        from pkg_resources import resource_filename
         with working_dir(os.path.dirname(self.modelpath)):
             try:
                 temp = NamedTemporaryFile(suffix='.g')
                 temp.write('include %s\n' % self.modelpath)
-                temp.write('\n'.join(self.extra_pars))
+                temp.write('include %s\n' %
+                           resource_filename('omv', 'backends/utils/genesis_utils.g'))
+                temp.write('\n'.join(self.extra_pars) + '\n')
                 temp.seek(0)
 
                 with open('/tmp/omv.gen.err', 'w+') as se:
@@ -58,8 +63,14 @@ class GenesisBackend(OMVBackend):
                 self.returncode = e.returncode
             finally:
                 temp.close()
-            
+
     def build_query_string(self, name, cmd):
-        return '{%s; echo %s: {%s}' % (cmd, name, name)
+        return 'echo %s: {%s}' % (name, cmd)
 
+    def query_area(self, secname):
+        name = self.register_query('area_%s' % secname.translate(None, '/[]'),
+                                   'total_membrane_area %s' % secname)
+        return name
 
+    def query_temperature(self):
+        return self.register_query('temperature', 'celsius')
