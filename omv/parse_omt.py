cb5d6615dc7b0d8ab291bdca76fa578c50f30831
Boris Marin <borismarin@gmail.com>
2014-09-15 18:25:59 +0100

    starting to create obj model for omt/mep defs

--- omv/parse_omt.py
+++ omv/parse_omt.py
@@ -1,43 +1,55 @@
 import yaml
 from backends import OMVBackends
 from analyzers import OMVAnalyzers
-from common.output import inform
 from os.path import join, dirname
+
+
+class OMVTestSuite(object):
+    def __init__(self, omt_path):
+        omt = load_yaml(omt_path)
+        omt_root = dirname(omt_path)
+        engine = omt['engine']
+        target = omt['target']
+        self.backend = OMVBackends[engine](join(omt_root, target))
+
+        impl = omt.get('implements')
+        if impl:
+            mepfile = join(omt_root, impl['mep'])
+            observables = impl['observables']
+            mep = load_yaml(mepfile)
+            experiment = mep['experiments'][impl['experiment']]
+        else:
+            observables = {'dry': None}
+            experiment = {'expected': {'dry': None}}
+        self.experiments = OMVExperiment(experiment,
+                                         observables, self.backend, omt_root)
+
+    def run(self):
+        self.backend.run()
+        return [check() for check in self.experiments.checks]
+
+
+class OMVExperiment(object):
+    def __init__(self, experiment, observables, backend, omt_root):
+        self.observables = observables
+        self.experiment = experiment
+        self.checks = []
+        for obsname, observable in observables.iteritems():
+            # an experiment can have multiple observables,
+            expected = experiment['expected'].get(obsname, None)
+            self.checks.append(OMVAnalyzers[obsname]
+                               (observable, expected, backend, omt_root))
+
 
 def load_yaml(fname):
     with open(fname) as f:
         y = yaml.safe_load(f)
     return y
 
-def parse_omt(omt_path):
-    omt = load_yaml(omt_path)
-    engine = omt['engine']
-    target = omt['target']
-    impl = omt.get('implements')
-
-    inform('--------------------------------')
-    inform('Running tests defined in:', omt_path)
-
-    tests = []
-    omt_root = dirname(omt_path)
-    if impl:
-        mepfile = join(omt_root, impl['mep'])
-        observables = impl['observables']
-        mep = load_yaml(mepfile) 
-        experiment = mep['experiments'][impl['experiment']]
-    else:
-        observables = {'dry':None} 
-        experiment = {'expected':{'dry':None}}
-
-    backend = OMVBackends[engine](join(omt_root, target))
-    for obsname, observable in observables.iteritems():
-        expected = experiment['expected'].get(obsname, None)
-        tests.append(OMVAnalyzers[obsname](observable, expected, backend, omt_root))
-
-    backend.run()
-    return [t() for t in tests]
     
-
+def parse_omt(omt_path):
+    return OMVTestSuite(omt_path).run()
+    
 
 if __name__ == '__main__':
     import sys
@@ -46,15 +58,5 @@ if __name__ == '__main__':
     exit(not all(t))
     
     
-
-
-
-
-
-
-
-
-
-
 
 
