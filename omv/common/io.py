97f90aa7228276f448c9374788c77730895b3d4c
Boris Marin <borismarin@gmail.com>
2014-09-18 01:26:30 +0100

    improved output formatting

--- omv/common/io.py
+++ omv/common/io.py
@@ -1,9 +1,33 @@
 import yaml
+import textwrap
+
+LINEWIDTH = 70
+PROMPT = '[omv] '
 
 
-def inform(msg, pars=[], indent=0):
-    p = pars if (pars is not None and pars != []) else ''
-    print 'OMV >>>  ' + '    ' * indent + msg, p
+def omvify(x):
+    return textwrap.TextWrapper(initial_indent=PROMPT,
+                                subsequent_indent=PROMPT).fill(x)
+
+
+def center(string):
+    nwhite = (LINEWIDTH - len(PROMPT) - len(string)) // 2
+    return nwhite * ' ' + string
+
+
+def underlined(string, char='-'):
+    rule = len(string) * char
+    return map(center, [string, rule])
+
+
+def inform(msg, pars=None, indent=0, underline=None):
+    if underline:
+        l = underlined(msg, underline)
+        msg = '\n'.join(map(omvify, l))
+    else:
+        p = pars if pars else ''
+        msg = omvify('  ' * indent + msg + p)
+    print msg
 
 
 def load_yaml(fname):
