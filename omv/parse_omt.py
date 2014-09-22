7efdf3a9205a0a20cee738252c5a89f1efe4efd3
Boris Marin <borismarin@gmail.com>
2014-09-22 20:21:24 +0100

    added tallyman class

--- omv/parse_omt.py
+++ omv/parse_omt.py
@@ -3,6 +3,7 @@ from backends.backend import BackendInstallationError, BackendExecutionError
 from omt_mep_parser import OMVTestParser
 from common.inout import inform, check
 from os.path import basename
+from tally import Tallyman
 
 
 def parse_omt(omt_path):
@@ -14,24 +15,26 @@ def parse_omt(omt_path):
     backend = OMVBackends[mepomt.engine](mepomt.modelpath)
     experiments = [exp for exp in mepomt.generate_exps(backend)]
     
-    results = [False]
+    tally = Tallyman(mepomt)
+    
     try:
         backend.run()
-        results = []
         for exp in experiments:
             inform('Running checks for experiment ', exp.name, indent=1)
             inform('')
-            res = exp.check_all()
-            results.append(res.values)
+            results = exp.check_all()
             inform('{:<30}{:^20}'.format('Observable', 'Test Passed'),
                    underline='-', indent=3)
-            for rn, rv in res.iteritems():
+            for rn, rv in results.iteritems():
                 inform(u'{:<30}{:^20}'.format(rn, check(rv)), indent=3)
+            tally.add_experiment(exp, results)
+
     except (BackendInstallationError, BackendExecutionError):
+        # TODO: serialize exception info
         inform('ERROR running backend ', backend.name, indent=1,
                underline='-', overline='-')
 
-    return results
+    return tally
     
     
 
