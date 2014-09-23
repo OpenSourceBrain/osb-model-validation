604fa58026b91d95f6a6eb430de7036900ae144e
Boris Marin <borismarin@gmail.com>
2014-09-23 19:30:20 +0100

    Added timestamp checking. Closes #38

--- omv/analyzers/spikes.py
+++ omv/analyzers/spikes.py
@@ -5,23 +5,34 @@ from ..common.inout import inform
 
 
 class SpikeAnalyzer(OMVAnalyzer):
-    
+
+    def before_running(self):
+        if 'file' in self.observable:
+            self.f = fn.FileNodeHelper(self.observable['file'], self.omt_root)
+            if self.f.tstamp:
+                inform('Attention! Preexistent datafile: ', self.f.filename,
+                       indent=2, verbosity=1)
+
     def parse_spikes(self, to_parse):
         if isinstance(to_parse, list):
             spikes = to_parse
         elif 'file' in to_parse:
-            f = fn.FileNodeHelper(to_parse['file'], self.omt_root)
-            tv = f.get_timeseries()
-            inform('Reading timeseries from:', f,
-                   indent=1, verbosity=1)
-            if 'spike detection' in to_parse:
-                sd = to_parse['spike detection']
-                method = sd.get('method', 'threshold')
-                threshold = float(sd.get('threshold', 0))
-                inform('Detecting spikes with method:',
-                       method, indent=2, verbosity=1)
-                spikes = ts.spikes_from_timeseries(tv, method=method,
-                                                   threshold=threshold)
+            if self.f.has_changed():
+                tv = self.f.get_timeseries()
+                inform('Reading timeseries from: ', self.f,
+                       indent=1, verbosity=1)
+                if 'spike detection' in to_parse:
+                    sd = to_parse['spike detection']
+                    method = sd.get('method', 'threshold')
+                    threshold = float(sd.get('threshold', 0))
+                    inform('Detecting spikes with method: ',
+                           method, indent=2, verbosity=1)
+                    spikes = ts.spikes_from_timeseries(tv, method=method,
+                                                       threshold=threshold)
+            else:
+                inform('ERROR! Preexistent datafile %s has not been updated!'
+                       % self.f.filename, indent=2, verbosity=0, underline='-')
+                spikes = []
         return spikes
 
     def parse_expected(self):
