be996249e3f1f469eec707c99087b19f2fd66b25
Boris Marin <borismarin@gmail.com>
2014-11-07 17:48:19 +0000

    custom simrc in getgenesis

--- omv/backends/getgenesis.py
+++ omv/backends/getgenesis.py
@@ -3,6 +3,37 @@ from subprocess import check_output as co
 
 from utils.wdir import working_dir
 
+simrc = """
+// .simrc file to go in the home directory of all GENESIS users
+//===========================================================================
+//                         environment variables
+//===========================================================================
+//      SIMPATH - path to search for scripts (includes the startup path)
+//      SIMNOTES - default file for simulator notes
+//===========================================================================
+setenv SIMPATH. {getenv HOME}/genesis/genesis2.4gamma-master/src/startup\
+{getenv HOME}/genesis/genesis2.4gamma-master/Scripts/neurokit   \
+{getenv HOME}/genesis/genesis2.4gamma-master/Scripts/neurokit/prototypes
+
+// This adds the Xodus 1 compatability library directory to the
+// SIMPATH.  If you are not expecting to use X1compat, you can
+// comment out the following line.
+setenv SIMPATH {getenv SIMPATH}\
+{getenv HOME}/genesis/genesis2.4gamma-master/Scripts/X1compat
+
+setenv SIMNOTES {getenv HOME}/genesis/genesis2.4gamma-master/.notes
+setenv GENESIS_HELP {getenv HOME}/genesis/genesis2.4gamma-master/Doc
+
+//===========================================================================
+//                             set up tables 
+//===========================================================================
+schedule// default simulation schedule
+
+// See /usr/genesis/startup/escapelist.g for macro definitions.  These allow
+// you to use cursor keys for the recall and editing of previous commands
+include          escapelist      // setup macro keys for DEC and SUN keyboards
+"""
+
 
 def install_genesis(get_latest=False):
     genpath = os.path.join(os.environ['HOME'], 'genesis')
@@ -15,9 +46,7 @@ def install_genesis(get_latest=False):
         os.chdir('genesis2.4gamma-master/src')
         print co(['./configure'])
         print co(['make', 'nxgenesis'])
-        print co(['cp', 'startup/travis_simrc',
-                  os.path.join(os.environ['HOME'], '.simrc')])
-
+        open(os.path.join(os.environ['HOME'], '.simrc'), 'w').write(simrc)
 
 
 
