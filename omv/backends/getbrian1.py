143913a2763d8e6173085584c9760fcd063ba21a
Padraig Gleeson <p.gleeson@gmail.com>
2014-11-19 13:12:57 +0000

    Test installation with conda

--- omv/backends/getbrian1.py
+++ omv/backends/getbrian1.py
@@ -4,12 +4,20 @@ from subprocess import check_output as co
 from ..common.inout import inform
 from utils.wdir import working_dir
 
+def execute(cmds):
+    print("Executing: %s"%cmds)
+    print(co(cmds))
+    
+
 def install_brian():
     
     install_root = os.environ['HOME']
     
     with working_dir(install_root):
-        print co(['sudo', 'apt-get', 'install', 'python-brian'])
+        execute(['sudo', 'pip', 'install', 'conda'])
+        execute(['sudo', 'conda', 'init'])
+        execute(['conda', 'create', '-p', '$HOME/py', '--yes', "'numpy scipy'"])
+        execute(['sudo', 'pip', 'install', 'brian'])
         import brian
         inform('Successfully installed Brian version %s'%brian.__version__, indent=2, verbosity=1)
         
