a38654cfbc750839608dd828580ec5fba50a9703
Padraig Gleeson <p.gleeson@gmail.com>
2014-11-19 11:56:41 +0000

    Try apt-get

--- omv/backends/getbrian1.py
+++ omv/backends/getbrian1.py
@@ -9,7 +9,7 @@ def install_brian():
     install_root = os.environ['HOME']
     
     with working_dir(install_root):
-        print co(['easy_install', 'brian'])
+        print co(['apt-get', 'install', 'python-brian'])
         import brian
         inform('Successfully installed Brian version %s'%brian.__version__, indent=2, verbosity=1)
         
