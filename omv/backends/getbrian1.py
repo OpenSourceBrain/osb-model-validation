8a9ec2b80682bab5a2da9eace5e2c85d4fdddab1
Boris Marin <borismarin@gmail.com>
2014-11-24 17:57:09 +0000

    installing scipy via pip for brian

--- omv/backends/getbrian1.py
+++ omv/backends/getbrian1.py
@@ -1,8 +1,19 @@
-import os
 import pip
+
+
+def install_scipy_dev():
+    try:
+        import scipy
+    except:
+        print 'Installing scipy dev...'
+        pip.main(['install', 'cython'])
+        pip.main(['install', 'git+http://github.com/scipy/scipy/'])
+        import scipy
+
 
 def install_brian():
     try:
+        install_scipy_dev()
         pip.main(['install', 'brian'])
         import brian
         m = 'Successfully installed Brian...'
