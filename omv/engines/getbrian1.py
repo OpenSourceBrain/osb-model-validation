38e688327fdfbbb744e23a1a8c37281b623ef70c
Padraig Gleeson <p.gleeson@gmail.com>
2015-09-21 18:50:47 +0100

    More info on errors with Brian install

--- omv/engines/getbrian1.py
+++ omv/engines/getbrian1.py
@@ -1,7 +1,7 @@
 import pip
 
 
-def install_scipy_dev():
+def check_scipy_dev():
     try:
         import scipy
     except ImportError:
@@ -9,13 +9,18 @@ def install_scipy_dev():
         #print 'Installing scipy dev...'
         #pip.main(['install', 'cython'])
         #pip.main(['install', 'git+http://github.com/scipy/scipy/'])
-        print("ERROR: Brian requires scipy. Please install it manually.")
+        print("\n***************************************************************************************")
+        print("\n* ")
+        print("\n**  ERROR: Brian requires scipy. Please install it manually.")
+        print("\n**  If running OMV on Travis-CI, see https://github.com/OpenSourceBrain/BrianShowcase/blob/master/.travis.yml.")
+        print("\n* ")
+        print("\n***************************************************************************************")
         raise ImportError
 
 
 def install_brian():
     try:
-        install_scipy_dev()
+        check_scipy_dev()
         pip.main(['install', 'brian'])
         import brian
         m = 'Successfully installed Brian...'
--- omv/engines/getbrian2.py
+++ omv/engines/getbrian2.py
@@ -1,21 +1,10 @@
 import pip
 
-
-def install_scipy_dev():
-    try:
-        import scipy
-    except ImportError:
-        # Compiling from source is terribly slow, and requires Blas/Lapack...
-        #print 'Installing scipy dev...'
-        #pip.main(['install', 'cython'])
-        #pip.main(['install', 'git+http://github.com/scipy/scipy/'])
-        print("ERROR: Brian2 requires scipy. Please install it manually.")
-        raise ImportError
-
+from getbrian1 import check_scipy_dev
 
 def install_brian2():
     try:
-        install_scipy_dev()
+        check_scipy_dev()
         pip.main(['install', 'brian2'])
         import brian2
         m = 'Successfully installed Brian2...'
