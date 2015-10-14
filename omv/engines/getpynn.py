483115d34817f44b7504ea1cca6e15eeaf5011f7
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-14 16:10:51 +0100

    More for pynn

--- omv/engines/getpynn.py
+++ omv/engines/getpynn.py
@@ -4,6 +4,7 @@ from ..common.inout import inform
 def install_pynn():
     try:
         pip.main(['install', 'lazyarray'])  # This should ideally be automatically installed with PyNN...
+        pip.main(['install', 'neo'])  # This should ideally be automatically installed with PyNN...
         pip.main(['install', 'pyNN'])
         import pyNN
         m = 'Successfully installed pyNN...'
