827545b2e2765af4c6ccb87237ddced67a03d291
Padraig Gleeson <p.gleeson@gmail.com>
2015-11-12 18:08:03 +0000

    Updating PyNN engine to use latest from master

--- omv/engines/getpynn.py
+++ omv/engines/getpynn.py
@@ -3,9 +3,19 @@ from ..common.inout import inform
 
 def install_pynn():
     try:
+        
         pip.main(['install', 'lazyarray'])  # This should ideally be automatically installed with PyNN...
         pip.main(['install', 'neo'])  # This should ideally be automatically installed with PyNN...
-        pip.main(['install', 'pyNN'])
+
+        install_root = os.environ['HOME']
+
+        with working_dir(install_root):
+            check_output(['git', 'clone', 'git@github.com:NeuralEnsemble/PyNN.git'])
+
+        path = os.path.join(install_root,'PyNN')
+
+        with working_dir(path):
+            check_output(['python', 'setup.py', 'install'])
         import pyNN
         m = 'Successfully installed pyNN...'
     except Exception as e:
