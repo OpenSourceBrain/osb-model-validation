70b45c05848c8d5685c4d5a600141d45d454c733
borismarin <borismarin@gmail.com>
2016-04-02 04:00:43 +0100

    corrected queries for pyneuron

--- omv/engines/pyneuron.py
+++ omv/engines/pyneuron.py
@@ -11,7 +11,7 @@ from engine import EngineExecutionError
 
 
 class PyNRNEngine(NeuronEngine):
-    
+
     name = "PyNEURON"
 
     @staticmethod
@@ -20,9 +20,9 @@ class PyNRNEngine(NeuronEngine):
             inform("Checking whether %s is installed correctly..." %
                    PyNRNEngine.name, indent=1)
         installed = NeuronEngine.is_installed(None)
-        
+
         return installed
-        
+
     @staticmethod
     def install(version):
         if not NeuronEngine.is_installed(None):
@@ -30,40 +30,39 @@ class PyNRNEngine(NeuronEngine):
             inform("%s installed NEURON..." % PyNRNEngine.name, indent=2, verbosity =1)
 
         environment_vars_nrn, path_nrn = NeuronEngine.get_nrn_environment()
-        
+
         PyNRNEngine.path = path_nrn
         PyNRNEngine.environment_vars = {}
         PyNRNEngine.environment_vars.update(environment_vars_nrn)
-            
+
         inform("PATH: " + PyNRNEngine.path, indent=2, verbosity =1)
         inform("Env vars: %s" % PyNRNEngine.environment_vars, indent=2, verbosity =1)
-        
+
+    def build_query_string(self, name, cmd):
+        return "h('" + super(PyNRNEngine, self).build_query_string(name, cmd) + "')"
 
 
     def run(self):
-        
+
         try:
             self.stdout = self.compile_modfiles()
         except sp.CalledProcessError as err:
             self.stderr = err.output
             self.returncode = err.returncode
             inform('Error compiling modfiles:', self.stderr, indent=2)
-            
+
         with working_dir(dirname(self.modelpath)):
-            
+
             inform("Running %s on %s..." % (self.name, self.modelpath),
                    indent=1)
             p = sp.Popen(['nrniv', '-python', self.modelpath, '-nogui'],
                          stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-            cmd = '''\
-            %s
-            ''' % ('\n'.join(self.extra_pars))
-            stdout, stderr = p.communicate(dedent(cmd))
+            stdout, stderr = p.communicate(dedent('\n'.join(self.extra_pars)))
             # with open('/tmp/omv_test.nrn.stdout', 'w') as f:
             #     f.write(stdout)
             self.stdout = stdout
             self.stderr = stderr
-            
+
             inform("OUT: ", stdout, verbosity=1, indent=2)
             inform("ERR: ", stderr, verbosity=1, indent=2)
             inform("returncode: ", p.returncode, verbosity=1, indent=2)
@@ -71,19 +70,6 @@ class PyNRNEngine(NeuronEngine):
             self.returncode = p.returncode
             if self.returncode is not 0:
                 raise EngineExecutionError
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
