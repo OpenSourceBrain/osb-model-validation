c8ed398fdfb077b2e56d55895180b62a6aad8470
Padraig Gleeson <p.gleeson@gmail.com>
2016-08-12 15:35:26 +0100

    Ading engine for neuroConstruct

--- omv/engines/pyneuroconstruct.py
+++ omv/engines/pyneuroconstruct.py
@@ -1,40 +1,67 @@
-
-import subprocess as sp
+import os
 
 from utils.wdir import working_dir
-from os.path import dirname
 
-
-from ..common.inout import inform, is_verbose
+from ..common.inout import inform, check_output as co, trim_path
 from engine import OMVEngine
 
 
 class PyneuroConstructEngine(OMVEngine):
     
     name = "Py_neuroConstruct"
+    
+    @staticmethod
+    def get_nC_environment():
+
+        nc_path = os.path.join(os.environ['HOME'],'neuroConstruct')
+        if os.environ.has_key('NC_HOME'):
+            nc_path = os.environ['NC_HOME']+'/'
+
+        environment_vars = {'NC_HOME': nc_path}
+
+        return environment_vars
 
     @staticmethod
     def is_installed(version):
         
-        # Check whether nC is installed by trying to run: nC.sh -version
-        
-        return False
+        inform('Checking whether neuroConstruct is installed', indent=2)
+        ret = True
+        try:
+            with working_dir(PyneuroConstructEngine.get_nC_environment()['NC_HOME']):
+                print co(['./nC.sh','-v'])
+        except Exception as err:
+            inform("Couldn't execute neuroConstruct:", err, indent=1)
+            ret = False
+        return ret
         
         
     @staticmethod
     def install(version):
 
         from getneuroconstruct import install_neuroconstruct
-
-        # Add install path (containing nC.sh) to PyneuroConstruct.path
+        
+        inform('Will fetch and install the latest neuroConstruct', indent=2)
+        install_neuroconstruct()
+        inform('Done...', indent=2)
 
 
     def run(self):
         
-        # Run the python interface to nC on the model file by constructing & running a command like:
-        # ['nC.sh', '-python', self.modelpath]
-        
-        self.returncode = -1 #  fail at present
+        try:
+            inform("Running file %s with Py_neuroConstruct" % trim_path(self.modelpath), indent=1)
+            
+            nC_sh = os.path.join(PyneuroConstructEngine.get_nC_environment()['NC_HOME'], 'nC.sh')
+            self.stdout = co([nC_sh, '-python', self.modelpath, '-nogui'],
+                                          cwd=os.path.dirname(self.modelpath))
+            self.returncode = 0
+        except sp.CalledProcessError as err:
+            self.returncode = err.returncode
+            self.stdout = err.output
+            raise EngineExecutionError
+        except Exception as err:
+            inform("Another error with running Py_neuroConstruct:", err, indent=1)
+            self.returncode = -1
+            self.stdout = "???"
 
 
 
