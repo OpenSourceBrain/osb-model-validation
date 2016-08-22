da5fab809d6273c6fe1a31340800e672b105207a
Padraig Gleeson <p.gleeson@gmail.com>
2016-08-22 15:19:05 +0100

    Better handling of paths

--- omv/engines/getpyneuroml.py
+++ omv/engines/getpyneuroml.py
@@ -4,14 +4,14 @@ from omv.common.inout import check_output as co
 from utils.wdir import working_dir
 
 def install_pynml():
-    pynmlpath = os.path.join(os.environ['HOME'],'pynml')
-    os.mkdir(pynmlpath)
-    with working_dir(pynmlpath):
+    
+    with working_dir(os.environ['HOME']):
         print(co(['git', 'clone', 'https://github.com/NeuralEnsemble/libNeuroML.git']))
-        print(co(['cd', 'libNeuroML']))
+        print(co(['git', 'clone', 'https://github.com/NeuroML/pyNeuroML.git']))
+        
+    with working_dir(os.path.join(os.environ['HOME'],'libNeuroML')):
         print(co(['git', 'checkout', 'development']))
         print(co(['python', 'setup.py', 'install']))
-        print(co(['cd', '..']))
-        print(co(['git', 'clone', 'https://github.com/NeuroML/pyNeuroML.git']))
-        print(co(['cd', 'pyNeuroML']))
+        
+    with working_dir(os.path.join(os.environ['HOME'],'pyNeuroML')):
         print(co(['python', 'setup.py', 'install']))
