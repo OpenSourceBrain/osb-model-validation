cb076e60bb41a423cf866ca5037b522f750493a4
Padraig Gleeson <p.gleeson@gmail.com>
2016-08-23 11:12:56 +0100

    Different install

--- omv/engines/getpyneuroml.py
+++ omv/engines/getpyneuroml.py
@@ -1,17 +1,14 @@
-import os
-from omv.common.inout import check_output as co
-
-from utils.wdir import working_dir
+import pip
+from omv.common.inout import inform
 
 def install_pynml():
     
-    with working_dir(os.environ['HOME']):
-        print(co(['git', 'clone', 'https://github.com/NeuralEnsemble/libNeuroML.git']))
-        print(co(['git', 'clone', 'https://github.com/NeuroML/pyNeuroML.git']))
-        
-    with working_dir(os.path.join(os.environ['HOME'],'libNeuroML')):
-        print(co(['git', 'checkout', 'development']))
-        print(co(['python', 'setup.py', 'install']))
-        
-    with working_dir(os.path.join(os.environ['HOME'],'pyNeuroML')):
-        print(co(['pip', 'install', '.', '-r', 'requirements.txt']))
+    try:
+        pip.main(['install', 'pyneuroml'])
+        import neuroml
+        import pyneuroml
+        m = 'Successfully installed pyNeuroML...'
+    except Exception as e:
+        m = 'ERROR installing pyNeuroML: ' + str(e)
+    finally:
+        inform(m, indent=2)
