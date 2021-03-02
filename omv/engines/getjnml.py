b33ca66d88e5bef9c78b68dbf23f94263dcab381
Padraig Gleeson <p.gleeson@gmail.com>
2021-03-02 17:30:11 +0000

    use jnml 0.10.2

--- omv/engines/getjnml.py
+++ omv/engines/getjnml.py
@@ -4,13 +4,13 @@ from subprocess import check_output
 from omv.engines.utils.wdir import working_dir
 
 def install_jnml():
-        
-    version='0.10.1'
-    
+
+    version='0.10.2'
+
     jnmlpath = os.path.join(os.environ['HOME'],'jnml')
     if not os.path.isdir(jnmlpath):
         os.mkdir(jnmlpath)
     with working_dir(jnmlpath):
-        
+
         check_output(['wget', 'https://github.com/NeuroML/jNeuroML/releases/download/v%s/jNeuroML.zip'%(version)])
         check_output(['unzip', 'jNeuroML.zip'])
