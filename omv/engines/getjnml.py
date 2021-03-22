7755a8e2878760426cc8615cc469cb3a18bce680
Padraig Gleeson <p.gleeson@gmail.com>
2021-03-22 15:32:24 +0000

    Update version of jneuroml

--- omv/engines/getjnml.py
+++ omv/engines/getjnml.py
@@ -5,12 +5,12 @@ from omv.engines.utils.wdir import working_dir
 
 def install_jnml():
 
-    version='0.10.2'
+    version='NMLv2.1'
 
     jnmlpath = os.path.join(os.environ['HOME'],'jnml')
     if not os.path.isdir(jnmlpath):
         os.mkdir(jnmlpath)
     with working_dir(jnmlpath):
 
-        check_output(['wget', 'https://github.com/NeuroML/jNeuroML/releases/download/v%s/jNeuroML.zip'%(version)])
+        check_output(['wget', 'https://github.com/NeuroML/jNeuroML/releases/download/%s/jNeuroML.zip'%(version)])
         check_output(['unzip', 'jNeuroML.zip'])
