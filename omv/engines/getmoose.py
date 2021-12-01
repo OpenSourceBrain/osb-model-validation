4c3cca4afe86f264cbb024851c811b5b7ebf58e4
Padraig Gleeson <p.gleeson@gmail.com>
2021-12-01 13:12:38 +0000

    Test updated moose

--- omv/engines/getmoose.py
+++ omv/engines/getmoose.py
@@ -5,21 +5,21 @@ from omv.engines.utils.wdir import working_dir
 
 def install_moose():
     install_root = os.environ['HOME']
-    
+
     with working_dir(install_root):
-        print(check_output(['git', 'clone', 'https://github.com/pgleeson/moose-core.git']))
+        print(check_output(['git', 'clone', 'https://github.com/OpenSourceBrain/moose-core.git']))
 
     with working_dir(os.path.join(install_root,'moose-core')):
+        print(check_output(['git', 'checkout', 'nml2_updates']))
         print(check_output(['mkdir', 'build_']))
-        
+
     with working_dir(os.path.join(install_root,'moose-core/build_')):
         print(check_output(['cmake', '..']))
         print(check_output(['make','-j4']))
         print(check_output(['make', 'install']))
-        
+
     with working_dir(os.path.join(install_root,'moose-core/build_/python')):
         print(check_output(['pwd']))
         print(check_output(['python', 'setup.py', 'install']))
 
-    m = 'Successfully installed Moose...' 
-        
+    m = 'Successfully installed Moose...'
