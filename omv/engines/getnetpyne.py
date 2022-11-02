3ec7fe7c2e592aec953a222ac68a9269bdaf8b7e
Padraig Gleeson <p.gleeson@gmail.com>
2022-11-02 11:38:35 +0000

    To v0.1.16; fix omv install netpyne

--- omv/engines/getnetpyne.py
+++ omv/engines/getnetpyne.py
@@ -4,31 +4,38 @@ from omv.common.inout import pip_install
 import os
 import sys
 
-def install_netpyne():
-    install_root = os.environ['HOME']
-    install_dir = 'netpyneInstall'
-    path = os.path.join(install_root,install_dir)
-    
-    if not os.path.isdir(path):
-        with working_dir(install_root):
-            print(check_output(['git', 'clone', 'https://github.com/Neurosim-lab/netpyne.git', install_dir]))
+def install_netpyne(version=None):
 
-    with working_dir(path):
-        print(check_output(['git', 'checkout', 'neuroml_updates']))
-        print(check_output(['pwd']))
-        
-    with working_dir(path):
-        if sys.version_info.major == 2:
-            #pip_install([]'setuptools==42.0.1')
-            pip_install(['kiwisolver==1.0.1',
-                         'matplotlib==2.2.4',
-                         'pandas==0.23.4',
-                         'bokeh==1.4.0', 
-                         'Pillow==5.4.1',
-                         'matplotlib-scalebar==0.5.1',
-                         'scipy==1.2.2',
-                         'python-dateutil==2.8.0'])
-        print(check_output([sys.executable, 'setup.py', 'install']))
+    if version is not None:
 
-    m = 'Successfully installed NetPyNE...'
+        pip_install('netpyne',version)
+        import netpyne
         
+    else:
+
+        install_root = os.environ['HOME']
+        install_dir = 'netpyneInstall'
+        path = os.path.join(install_root,install_dir)
+
+        if not os.path.isdir(path):
+            with working_dir(install_root):
+                print(check_output(['git', 'clone', 'https://github.com/Neurosim-lab/netpyne.git', install_dir]))
+
+        with working_dir(path):
+            print(check_output(['git', 'checkout', 'neuroml_updates']))
+            print(check_output(['pwd']))
+
+        with working_dir(path):
+            if sys.version_info.major == 2:
+                #pip_install([]'setuptools==42.0.1')
+                pip_install(['kiwisolver==1.0.1',
+                             'matplotlib==2.2.4',
+                             'pandas==0.23.4',
+                             'bokeh==1.4.0',
+                             'Pillow==5.4.1',
+                             'matplotlib-scalebar==0.5.1',
+                             'scipy==1.2.2',
+                             'python-dateutil==2.8.0'])
+            print(check_output([sys.executable, 'setup.py', 'install']))
+
+        m = 'Successfully installed NetPyNE...'
