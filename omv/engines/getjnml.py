861198e4e2fc2f8e5c53d9decaa29627d62ceced
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-07-04 11:52:52 +0100

    chore(jnml): format with black

--- omv/engines/getjnml.py
+++ omv/engines/getjnml.py
@@ -7,27 +7,32 @@ from omv.engines.utils.wdir import working_dir
 
 
 def install_jnml():
-
-    version='v0.12.2'
+    version = "v0.12.2"
 
     try:
-        jnmlhome = os.environ['JNML_HOME']
+        jnmlhome = os.environ["JNML_HOME"]
         jnmlpath = Path(jnmlhome).parent
     except KeyError:
         osname = platform.system()
         if osname == "Linux":
             try:
-                jnmlpath = os.path.join(os.environ['XDG_DATA_HOME'],'jnml')
+                jnmlpath = os.path.join(os.environ["XDG_DATA_HOME"], "jnml")
             except KeyError:
-                jnmlpath = os.path.join(os.environ['HOME'],'.local/share/jnml')
+                jnmlpath = os.path.join(os.environ["HOME"], ".local/share/jnml")
         elif osname == "Darwin":
-            jnmlpath = os.path.join(os.environ['HOME'],'Library/jnml')
+            jnmlpath = os.path.join(os.environ["HOME"], "Library/jnml")
         else:
-            jnmlpath = os.path.join(os.environ['HOME'],'jnml')
+            jnmlpath = os.path.join(os.environ["HOME"], "jnml")
 
     if not os.path.isdir(jnmlpath):
         os.mkdir(jnmlpath)
     with working_dir(jnmlpath):
-
-        check_output(['wget', '-nv', 'https://github.com/NeuroML/jNeuroML/releases/download/%s/jNeuroML.zip'%(version)])
-        check_output(['unzip', 'jNeuroML.zip'])
+        check_output(
+            [
+                "wget",
+                "-nv",
+                "https://github.com/NeuroML/jNeuroML/releases/download/%s/jNeuroML.zip"
+                % (version),
+            ]
+        )
+        check_output(["unzip", "jNeuroML.zip"])
