f34764521edb7dc6dd7b035f53213f470816c844
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-06-11 17:02:42 +0100

    chore: do not use `all` for variable name

--- omv/engines/utils/__init__.py
+++ omv/engines/utils/__init__.py
@@ -10,11 +10,11 @@ def resolve_paths(path_s):
         import glob
 
         if PATH_DELIMITER in path_s:
-            all = []
+            all_paths = []
             for p in path_s.split(PATH_DELIMITER):
                 for g in glob.glob(p):
-                    all.append(g)
-            path_s = all
+                    all_paths.append(g)
+            path_s = all_paths
         else:
             path_s = glob.glob(path_s)
 
