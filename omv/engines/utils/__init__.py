6d0f58a2d4c8e01cf991de537eb70d1c3bef9d25
pgleeson <p.gleeson@gmail.com>
2025-06-19 18:28:25 +0100

    Fix to finding multiple files to validate in omv files

--- omv/engines/utils/__init__.py
+++ omv/engines/utils/__init__.py
@@ -6,16 +6,16 @@ def resolve_paths(path_s):
     Make explicit list from: '*.nml myfile.xml' etc.
     """
 
-    if "*" in path_s:
-        import glob
+    import glob
 
-        if PATH_DELIMITER in path_s:
-            all_paths = []
-            for p in path_s.split(PATH_DELIMITER):
-                for g in glob.glob(p):
-                    all_paths.append(g)
-            path_s = all_paths
-        else:
-            path_s = glob.glob(path_s)
+    if PATH_DELIMITER in path_s:
+        all_paths = []
+        for p in path_s.split(PATH_DELIMITER):
+            for g in glob.glob(p):
+                print('Found path:', g)
+                all_paths.append(g)
+        path_s = all_paths
+    else:
+        path_s = resolve_paths(glob.glob(path_s))
 
     return path_s
