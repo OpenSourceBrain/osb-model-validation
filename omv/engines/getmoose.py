f3c370574a37483e9df421c4843cec8fc6b19e65
Padraig Gleeson <p.gleeson@gmail.com>
2026-03-20 12:03:00 +0000

    Temp disable moose test...

--- omv/engines/getmoose.py
+++ omv/engines/getmoose.py
@@ -4,7 +4,7 @@ from omv.common.inout import pip_install
 def install_moose(version):
     if not version:
         version = "4.0.0.dev20240411"
-        version = "4.1.4"
+        version = "4.1.4" # temp test this, it is the latest on pypi... 
     try:
         pip_install("pymoose", version)
         pip_install("pint")
